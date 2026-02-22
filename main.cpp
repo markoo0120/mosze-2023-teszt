5b55239d7fdd449f728f5b138ca836a12867931d
markoo0120 <mestermark2005@gmail.com>
2026-02-22 20:32:27 +0100

    javitott

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS-nek kell lennie
-    std::cout << '1-100 ertekek duplazasa' // "" lenne a jó és hiányzik a ;
-    for (int i = 0;)// nincsen feltétel és léptető
+    int *b = new int[N_ELEMENTS]; //N_ELEMENTS-nek kell lennie
+    std::cout << "1-100 ertekek duplazasa" <<std::endl; // "" lenne a jó és hiányzik a ;
+    for (int i = 0; i < N_ELEMENTS; i++)// nincsen feltétel és léptető
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // hibás feltétel 
+    for (int i = 0; i < N_ELEMENTS ; i++) // hibás feltétel 
     {
-        std::cout << "Ertek:" // ; hiánya
+        std::cout << "Ertek:" << b[i] << std::endl; // ; hiánya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)//; helyett , van
+    int atlag=0;
+    for (int i = 0; i < N_ELEMENTS; i++)//; helyett , van
     {
-        atlag += b[i] // ; hiánya
+        atlag += b[i]; // ; hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; 
     return 0;
 }
